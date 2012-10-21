2b55c7b0fe9e59cd7e30b1945b3a7039b2b1ab49
Nik Gomez <niksterg11@gmail.com>
2012-10-21 16:33:56 -0400

    removed need for node server :(

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 $(function() {
-	$.getJSON('http://127.0.0.1:1337/?callback=?');
+	// $.getJSON('http://127.0.0.1:1337/?callback=?');
+	$.getJSON('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=niksterg11&api_key=424fad8d35ab6e59ab75c64c588262b9&format=json', function(response) {
+		handleData(response);
+	})
 });
 
 function handleData(data) {
-	this.trackList = new TrackList(data.rss.channel[0].item);
+	this.trackList = new TrackList(data.recenttracks.track);
 	this.trackListView = new TrackListView({model:this.trackList});
 	$('#nowplaying').html(this.trackListView.render().el);
 }
